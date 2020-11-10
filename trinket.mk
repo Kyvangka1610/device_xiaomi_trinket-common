2228c7555486d39520e054c35304ec68dc8b1e64
bgcngm <bgcngm@gmail.com>
2020-11-10 16:37:39 +0530

    trinket-common: Add MIUI's Offline Charging Animation
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- trinket.mk
+++ trinket.mk
@@ -97,6 +97,29 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Charger
+PRODUCT_COPY_FILES += \
+    device/xiaomi/trinket-common/charger/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
+    device/xiaomi/trinket-common/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    device/xiaomi/trinket-common/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    device/xiaomi/trinket-common/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    device/xiaomi/trinket-common/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    device/xiaomi/trinket-common/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    device/xiaomi/trinket-common/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    device/xiaomi/trinket-common/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    device/xiaomi/trinket-common/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    device/xiaomi/trinket-common/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    device/xiaomi/trinket-common/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    device/xiaomi/trinket-common/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    device/xiaomi/trinket-common/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    device/xiaomi/trinket-common/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    device/xiaomi/trinket-common/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    device/xiaomi/trinket-common/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    device/xiaomi/trinket-common/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    device/xiaomi/trinket-common/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    device/xiaomi/trinket-common/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    device/xiaomi/trinket-common/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png
+
 # Display/Graphics
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
